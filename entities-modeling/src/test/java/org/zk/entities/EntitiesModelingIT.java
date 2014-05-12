09744dffedf4fcbce1c8466932cd2507db95f15a
aurelije <aurelije@gmail.com>
2014-05-12 15:43:37 +0200

    Started implementing postSaveSuccessfulAll test.

--- entities-modeling/src/test/java/org/zk/entities/EntitiesModelingIT.java
+++ entities-modeling/src/test/java/org/zk/entities/EntitiesModelingIT.java
@@ -1,11 +1,9 @@
-package org.zk.view;
+package org.zk.entities;
 
 import org.fest.assertions.api.Assertions;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.zk.entities.Author;
-import org.zk.entities.Post;
 
 import javax.persistence.*;
 
@@ -59,6 +57,59 @@ public class EntitiesModelingIT {
         entityManager.getTransaction().commit();
         entityManager.close();
     }
+
+    @Test
+    public void postSaveSuccessfulAll() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        Post post = new Post();
+        Author author = new Author();
+
+        author.setUserName("testUser");
+        author.setUserPassword("password");
+
+        entityManager.persist(author);
+
+        PostComment comment = new PostComment();
+        comment.setAuthor(author);
+        comment.setContent("This is a post");
+
+        post.addComment(comment); // no need to persist comment, cascade will do the trick!
+
+        Tag testTag = new Tag();
+        testTag.setName("Test"); // no need to persist tag, cascade will do the trick!
+
+        post.addTag(testTag);
+
+        Image image1 = new Image("image title1", "illustration1.jpg", 200, 300);
+        Image image2 = new Image("image title2", "illustration2.jpg", 100, 80);
+
+        post.addImage(image1);
+        post.addImage(image2);
+
+        post.setAuthor(author);
+        post.setSubject("subject");
+        post.setBody("This is a body\nof this blog post");
+
+        entityManager.persist(post);
+
+        Long postId = post.getId();
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+
+        entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        Post foundPost = entityManager.find(Post.class, postId);
+
+        Assertions.assertThat(post.equals(foundPost)).isTrue();
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+
 
     @Test(expected = PersistenceException.class)
     public void postSaveUnsuccessfulNoAuthor() {
